28e7882f3dbeabc253755275a3a8f7bf43e3bddf
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2024-08-24 01:55:47 +0330

    Parallelized sampler

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use debuzzy::filter::*;
 use debuzzy::instrument::*;
 use debuzzy::mml;
 use debuzzy::sampler::*;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::io::Write;
 
 const SAMPLE_RATE: f64 = 44100.0;
@@ -17,13 +18,23 @@ impl Player for StdoutPlayer {
         let sample_rate_step = 1f64 / sample_rate;
         let mut t = 0f64;
         while t < duration {
-            let val = (sampler.sample(t) * 32767.0) as i16;
-            std::io::stdout().write(&val.to_le_bytes()).unwrap();
-            t += sample_rate_step;
+            // Sample one second
+            let samples = (0..SAMPLE_RATE as usize)
+                .into_par_iter()
+                .map(|i| (sampler.sample(t + sample_rate_step * (i as f64)) * 32767.0) as i16)
+                .collect::<Vec<_>>();
+            for val in samples {
+                std::io::stdout().write(&val.to_le_bytes()).unwrap();
+            }
+            t += sample_rate_step * (SAMPLE_RATE as f64);
         }
     }
 }
 
 fn main() {
-    StdoutPlayer::play(mml::play::<LegitInstrument>(mml::MARIO), SAMPLE_RATE, 100.0);
+    StdoutPlayer::play(
+        mml::play::<LegitInstrument>(mml::SMOKE_ON_THE_WATER),
+        SAMPLE_RATE,
+        100.0,
+    );
 }
